c1d3eecc3f9ae2f149c417ee18e30c259ee8a391
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-05-15 17:15:03 +0300

    Initial commit

---
+++ app/src/main/java/com/kirillmesh/composition/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kirillmesh.composition.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kirillmesh.composition.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
